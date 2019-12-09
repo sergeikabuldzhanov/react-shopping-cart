e6b507ad2b9642c54a40f9e27a9395e265f64d9b
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-09 19:08:20 +0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+import useLocalStorage from './hooks/useLocalStorage';
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+
+//Contexts
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useLocalStorage("cart", []);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    const productIsInCart = cart.find(product => product.id == item.id);
+    const currentNumberInCart = productIsInCart
+      ? productIsInCart.numberInCart+1
+	  : 1;
+    setCart([...cart, { ...item, numberInCart: currentNumberInCart }]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  const removeItem = index => {	  	  
+    setCart(cart.filter((cartElement, i)=> i!=index));
+  };
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  return (
+    <div className="App">
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{cart, removeItem}}>
+          <Navigation />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
