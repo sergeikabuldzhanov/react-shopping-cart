6d4512c4482462288e806ef310d7573197b489a5
sergeikabuldzhanov <thesiegebot@gmail.com>
2019-12-09 19:06:37 +0500

    Created custom hook to save cart in local storage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+ const useLocalStorage = (key, initialValue) =>{
+    const [storedValue, setStoredValue] = useState(()=>{
+        const isItemInStorage = window.localStorage.getItem(key);
+        console.log(isItemInStorage);
+        
+        return isItemInStorage ? JSON.parse(isItemInStorage) : initialValue;
+    });
+    const setValue = value =>{
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
